d1106d50eddd2f23364967ee30408e79d5e2b16c
Zian Choudhury <zchoudhury@myseneca.ca>
2023-10-24 21:46:30 -0400

    limit and skip params

--- src/main/java/com/app/dashboard/controllers/ShelterController.java
+++ src/main/java/com/app/dashboard/controllers/ShelterController.java
@@ -2,11 +2,9 @@ package com.app.dashboard.controllers;
 
 import com.app.dashboard.ShelterRepository;
 import com.app.dashboard.models.Shelter;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
@@ -28,7 +26,16 @@ public class ShelterController {
 
     @GetMapping("/shelters")
     @CrossOrigin
-    public List<Shelter> getAllShelters(){
-        return repository.getSheltersPaganeted(10, 10);
+    @ApiOperation(value = "Get all Shelters", notes = "Get a list of all Shelters optionally giving a limit and offset, default is first 50.")
+    public List<Shelter> getAllShelters(
+            @RequestParam(required = false) Integer limit,
+            @RequestParam(required = false) Integer skip
+    ) {
+        // Check if limit and skip parameters are provided and use default values if not
+        int limitValue = (limit != null) ? limit : 50;
+        int skipValue = (skip != null) ? skip : 0;
+
+        // Call the repository method with the limit and skip values
+        return repository.getSheltersPaganeted(skipValue, limitValue);
     }
 }
